63dee34f24ba1c8ced1b556eab6891691a1fb478
sandi.bhirama <sbhirama@gmail.com>
2019-10-25 18:28:31 +0700

    test

---
+++ src/test/java/apap/latihan/uts/UtsApplicationTests.java
@@ -0,0 +1,16 @@
+package apap.latihan.uts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UtsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
